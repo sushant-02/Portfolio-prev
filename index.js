3ddf1afc886e45a4e2651e4e383509450a53e0f1
Sushant Pandey <56975174+sushant-02@users.noreply.github.com>
2020-07-26 23:44:44 +0530

    Complete and responsive

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
-var app = document.getElementById('app');
+var type = document.getElementById('app');
 
-var typewriter = new Typewriter(app, {
-    loop: true,
-    cursor: "|"
+var typewriter = new Typewriter(type, {
+  loop: true,
+  cursor: "|"
 });
 
 typewriter.typeString('Hi, I\'m Sushant!')
-    .pauseFor(2000)
-    .deleteAll()
-    .typeString('I love coding')
-    .pauseFor(2000)
-    .deleteAll()
-    .typeString('I am a web designer')
-    .pauseFor(2000)
-    .deleteChars(8)
-    .typeString('developer')
-    .pauseFor(2000)
-    .start();
+  .pauseFor(2000)
+  .deleteAll()
+  .typeString('I love coding')
+  .pauseFor(2000)
+  .deleteAll()
+  .typeString('I am a web designer')
+  .pauseFor(2000)
+  .deleteChars(8)
+  .typeString('developer')
+  .pauseFor(2000)
+  .start();
+
+document.querySelector('.navbar__btn--cover').addEventListener('click', toggleDropDown);
+
+function toggleDropDown() {
+  document.getElementById('dropdown').classList.toggle('show');
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.navbar__btn--cover')) {
+  var dropdown = document.getElementById('dropdown');
+    if (dropdown.classList.contains('show')) {
+      dropdown.classList.remove('show');
+    }
+  }
+}
